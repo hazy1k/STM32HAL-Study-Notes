548c6101c145a5debb414ebb1ef493ee8678564b
hazy1k <160259482+hazy1k@users.noreply.github.com>
2024-09-10 21:54:17 +0800

    code

--- 3.参考例程/0.新建工程模板(HAL)/USER/main.c
+++ 3.参考例程/0.新建工程模板(HAL)/USER/main.c
@@ -14,24 +14,24 @@ int main(void)
 
 	GPIO_InitTypeDef GPIO_Initure;
      
-   HAL_Init();                    // 初始化HAL库    
-   Stm32_Clock_Init(336,8,2,7);   // 设置时钟,168Mhz
+	HAL_Init();                    // 初始化HAL库    
+	Stm32_Clock_Init(336,8,2,7);   // 设置时钟,168Mhz
 
     __HAL_RCC_GPIOF_CLK_ENABLE(); // 开启GPIOF时钟
 	
-    GPIO_Initure.Pin=GPIO_PIN_9|GPIO_PIN_10; 	//PF9,10
-    GPIO_Initure.Mode=GPIO_MODE_OUTPUT_PP;  	//推挽输出
-    GPIO_Initure.Pull=GPIO_PULLUP;          	//上拉
-    GPIO_Initure.Speed=GPIO_SPEED_HIGH;    	 	//高速
-    HAL_GPIO_Init(GPIOF,&GPIO_Initure);
+    GPIO_Initure.Pin = GPIO_PIN_9|GPIO_PIN_10; 	//PF9,10
+    GPIO_Initure.Mode = GPIO_MODE_OUTPUT_PP;  	//推挽输出
+    GPIO_Initure.Pull = GPIO_PULLUP;          	//上拉
+    GPIO_Initure.Speed = GPIO_SPEED_HIGH;    	 	//高速
+    HAL_GPIO_Init(GPIOF, &GPIO_Initure);
 
 	while(1)
 	{
-		HAL_GPIO_WritePin(GPIOF,GPIO_PIN_9,GPIO_PIN_SET);		//PF9置1 
-		HAL_GPIO_WritePin(GPIOF,GPIO_PIN_10,GPIO_PIN_SET);		//PF10置1  			
+		HAL_GPIO_WritePin(GPIOF,GPIO_PIN_9,GPIO_PIN_SET); // PF9置1 
+		HAL_GPIO_WritePin(GPIOF,GPIO_PIN_10,GPIO_PIN_SET);// PF10置1  			
 		Delay(0x7FFFFF);
-		HAL_GPIO_WritePin(GPIOF,GPIO_PIN_9,GPIO_PIN_RESET);		//PF9置0
-		HAL_GPIO_WritePin(GPIOF,GPIO_PIN_10,GPIO_PIN_RESET);	//PF10置0  
+		HAL_GPIO_WritePin(GPIOF,GPIO_PIN_9,GPIO_PIN_RESET);	// PF9置0
+		HAL_GPIO_WritePin(GPIOF,GPIO_PIN_10,GPIO_PIN_RESET);// PF10置0  
 		Delay(0x7FFFFF);
 	}
 }
--- 3.参考例程/0.新建工程模板(HAL)/readme.txt
+++ 3.参考例程/0.新建工程模板(HAL)/readme.txt
@@ -22,17 +22,3 @@
 ⑤system_stm32f4xx.c，stm32f4xx_it.c, stm32f4xx_hal_msp.c 和main.c          ->   \STM32Cube_FW_F4_V1.10.0\Projects\STM32F429I-Discovery\Templates\Src
 
 SYSTEM文件夹，任何一个HAL库实验工程中有
-	
-注意事项:
-	无.
-
-
-					正点原子@ALIENTEK
-					2016-6-24
-					广州市星翼电子科技有限公司
-					电话：020-38271790
-					传真：020-36773971
-					购买：http://shop62103354.taobao.com
-					http://shop62057469.taobao.com
-					公司网站：www.alientek.com
-					技术论坛：www.openedv.com
